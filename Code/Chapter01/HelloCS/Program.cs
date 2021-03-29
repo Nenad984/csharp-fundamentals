36fa1a5639b21e22b27c1fa45b988e887ae3fe45
Nenad984 <risticnenad.vr@gmail.com>
2021-03-29 19:20:41 +0200

    New net5.0 feature records

--- Code/Chapter01/HelloCS/Program.cs
+++ Code/Chapter01/HelloCS/Program.cs
@@ -7,6 +7,16 @@ namespace HelloCS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int z;
+            var person = new Person("Nenad", 25);
+
+
+            Console.WriteLine(person.Name);
+
+
         }
+
+        public record Person(string Name, int Age);
     }
 }
