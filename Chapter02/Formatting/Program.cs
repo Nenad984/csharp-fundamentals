75b307980ffd3d1e4541dd5dffe576737e8c1380
Nenad984 <risticnenad.vr@gmail.com>
2021-04-04 14:57:03 +0200

    Added vscode

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -24,6 +24,40 @@ namespace Formatting
            Console.WriteLine(formatted); 
 
            Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C} ");
+
+           string applesText = "Apples";
+           int applesCount = 1234;
+           string bananasText = "Bananas";
+           int bananasCount = 56789;
+
+           Console.WriteLine(
+               format: "{0, -8} {1, 6:N0}",
+               arg0: "Name",
+               arg1: "Count"
+           );
+
+           Console.WriteLine(
+               format: "{0, -8} {1, 6:N0}",
+               arg0: applesText,
+               arg1: applesCount
+           );   
+
+           Console.Write("Type your first name and press ENTER: ");
+
+           string firstName = Console.ReadLine();
+
+           Console.Write("Type your first age and press ENTER: ");
+
+           string age = Console.ReadLine();
+
+           Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+
+           
+
+
+
+           
+          
         }
     }
 }
