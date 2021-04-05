7bb8c157f813522609733902132c53998de0d3a1
Nenad984 <risticnenad.vr@gmail.com>
2021-04-05 19:47:07 +0200

    Adding while, do statements

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using static System.Console;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            while (x < 10)
+            {
+                WriteLine(x);
+                x++;
+            }
+
+            string password;
+            int attempts = 5;
+            do
+            {
+                WriteLine("Enter your password");
+                attempts--;
+                password = ReadLine();
+
+            } while (password != "Pa$$w0rd" && attempts != 0);
+
+            if (attempts == 0)
+            {
+                WriteLine("Wrong password");
+            }
+            else
+            {
+                WriteLine("Correct!");
+            }
+
+
+
+        }
+    }
+}
