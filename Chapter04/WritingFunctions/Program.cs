75baf33303ae25acd8b07add3e8af7e3ac7a5db8
Nenad984 <risticnenad.vr@gmail.com>
2021-04-06 17:10:48 +0200

    Adding mathematical function, recursion and XML comments

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -8,15 +8,17 @@ namespace WritingFunctions
     {
         static void Main(string[] args)
         {
-            RunCalculateTax();
-           // RunTimesTable();
+            RunFactorial();
+            //RunCardinalToOrdinal();
+            // RunCalculateTax();
+            // RunTimesTable();
         }
 
         static void TimesTable(byte number)
         {
             WriteLine($"This is the {number} times table");
 
-            for(int row = 1; row <= 12; row ++)
+            for (int row = 1; row <= 12; row++)
             {
                 WriteLine($"{row} x {number} = {row * number}");
             }
@@ -34,23 +36,23 @@ namespace WritingFunctions
 
                 isNumber = byte.TryParse(ReadLine(), out byte number);
 
-                if(isNumber)
+                if (isNumber)
                 {
-                    TimesTable(number);   
+                    TimesTable(number);
                 }
                 else
                 {
                     WriteLine("You did not enter a valid number!");
                 }
-               
+
             } while (isNumber);
         }
 
         static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
         {
             decimal rate = 0.0M;
-            
-            switch(twoLetterRegionCode)
+
+            switch (twoLetterRegionCode)
             {
                 case "CH":// Switzerland
                     rate = 0.08M;
@@ -65,12 +67,12 @@ namespace WritingFunctions
                     break;
                 case "HU": // Hungary
                     rate = 0.27M;
-                    break;  
+                    break;
                 case "OR":// Oregon
                 case "AK":// Alaska
                 case "MT": // Montana
                     rate = 0.0M;
-                    break;  
+                    break;
                 case "ND":// North Dakota
                 case "WI":// Wisconsin
                 case "ME":// Maryland
@@ -82,7 +84,7 @@ namespace WritingFunctions
                     break;
                 default: // most US states
                     rate = 0.06M;
-                    break;                
+                    break;
             }
             return amount * rate;
         }
@@ -95,7 +97,7 @@ namespace WritingFunctions
             Write("Enter a two-letter region code: ");
             string region = ReadLine();
 
-            if(decimal.TryParse(amountInText, out decimal amount))
+            if (decimal.TryParse(amountInText, out decimal amount))
             {
                 decimal taxToPay = CalculateTax(amount, region);
                 WriteLine($"You must pay {taxToPay} in sales tax.");
@@ -105,6 +107,90 @@ namespace WritingFunctions
                 WriteLine("You did not enter a valid amount!");
             }
 
+        }
+
+        /// <summary>
+        /// Pass a 32-bit integer and it will be converted into its ordinal equivalent.
+        /// </summary>
+        /// <param name="number">Number is a cardinal value e.g. 1, 2, 3, and so on.</param>
+        /// <returns><returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on.</returns>
+        static string CardinalToOrdinal(int number)
+        {
+            switch (number)
+            {
+
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+
+                default:
+                    string numberAsText = number.ToString();
+                    char lastDigit = numberAsText[numberAsText.Length - 1];
+                    string suffix;
+
+                    switch (lastDigit)
+                    {
+                        case '1':
+                            suffix = "st";
+                            break;
+                        case '2':
+                            suffix = "nd";
+                            break;
+                        case '3':
+                            suffix = "rd";
+                            break;
+                        default:
+                            suffix = "th";
+                            break;
+                    }
+
+                    return $"{number}{suffix}";
+            }
+        }
+        static void RunCardinalToOrdinal()
+        {
+            for (int number = 0; number < 40; number++)
+            {
+                Write($"{CardinalToOrdinal(number)} ");
+            }
+            WriteLine();
+        }
+
+        static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+
+        static void RunFactorial()
+        {
+            bool isNumber;
+            do
+            {
+                Write("Enter a number: ");
+
+                isNumber = int.TryParse(ReadLine(), out int number);
+
+                if (isNumber)
+                {
+                    WriteLine($"{number:N0}! = {Factorial(number):N0}");
+                }
+                else
+                {
+                    WriteLine("You did not enter a valid number!");
+                }
+            } while (isNumber);
         }
     }
 }
