ff28a89dcb9644976280b42dfe5890b15e92ec27
Nenad984 <risticnenad.vr@gmail.com>
2021-04-09 09:26:18 +0000

    Working with numbers and text

---
+++ Chapter08/WorkingWithText/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using static System.Console;
+
+namespace WorkingWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "London";
+
+            WriteLine($"{city} is {city.Length} characters long");
+
+            WriteLine($"First char is {city[0]} and third is {city[2]}.");
+
+            
+
+        }
+    }
+}
