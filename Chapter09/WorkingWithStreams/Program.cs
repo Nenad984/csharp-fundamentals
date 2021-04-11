4619788c5e9b2b77bc1b88e9a5d66d2326391102
Nenad984 <risticnenad.vr@gmail.com>
2021-04-11 14:33:14 +0200

    Working with, files, folders and streams

---
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.IO.Compression;
+
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+namespace WorkingWithStreams
+{
+    class Program
+    {
+        static string[] callsigns = new string[]{
+              "Husker", "Starbuck", "Apollo", "Boomer", "Bulldog", "Athena", "Helo", "Racetrack"
+          };
+        static void Main(string[] args)
+        {
+            // WorkingWithText();
+            //WorkWithXml();
+            WorkWithCompression();
+        }
+
+        static void WorkingWithText()
+        {
+            string textFile = Combine(CurrentDirectory, "streams.txt");
+
+            StreamWriter text = File.CreateText(textFile);
+
+            foreach (string item in callsigns)
+            {
+                text.WriteLine(item);
+            }
+
+            text.Close();
+
+            // output the contents of the file 
+            WriteLine("{0} contains {1:N0} bytes.", arg0: textFile,
+                arg1: new FileInfo(textFile).Length);
+
+            WriteLine(File.ReadAllText(textFile));
+        }
+
+        static void WorkWithXml()
+        {
+            FileStream xmlFileStream = null;
+
+            XmlWriter xml = null;
+
+            try
+            {
+                string xmlFile = Combine(CurrentDirectory, "streams.xml");
+
+                xmlFileStream = File.Create(xmlFile);
+
+                xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+
+                // write the XML declaration
+                xml.WriteStartDocument();
+
+                // write a root element
+                xml.WriteStartElement("callsigns");
+
+                foreach (string item in callsigns)
+                {
+                    xml.WriteElementString("callsign", item);
+                }
+
+                xml.WriteEndElement();
+
+                xml.Close();
+
+                xmlFileStream.Close();
+
+                // output all the contents of the file 
+                WriteLine("{0} contains {1:N0} bytes.", arg0: xmlFile, arg1: new FileInfo(xmlFile).Length);
+
+                WriteLine(File.ReadAllText(xmlFile));
+
+            }
+            catch (Exception ex)
+            {
+                // if the path doesn't exist the exception will be caught
+                WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            finally
+            {
+                if (xml != null)
+                {
+                    xml.Dispose();
+
+                    WriteLine("The XML writer's unmanaged resources have been disposed.");
+                }
+
+                if (xmlFileStream != null)
+                {
+                    xmlFileStream.Dispose();
+                    WriteLine("The file stream's unmanaged resources have been disposed.");
+                }
+
+            }
+
+        }
+
+        static void WorkWithCompression(bool useBrotli = true)
+    {
+      string fileExt = useBrotli ? "brotli" : "gzip";
+
+      // compress the XML output
+      string filePath = Combine(
+        CurrentDirectory, $"streams.{fileExt}");
+
+      FileStream file = File.Create(filePath);
+
+      Stream compressor;
+
+      if (useBrotli)
+      {
+        compressor = new BrotliStream(file, CompressionMode.Compress);
+      }
+      else
+      {
+        compressor = new GZipStream(file, CompressionMode.Compress);
+      }
+
+      using (compressor)
+      {
+        using (XmlWriter xml = XmlWriter.Create(compressor))
+        {
+          xml.WriteStartDocument();
+          xml.WriteStartElement("callsigns");
+
+          foreach (string item in callsigns)
+          {
+            xml.WriteElementString("callsign", item);
+          }
+          // the normal call to WriteEndElement is not necessary
+          // because when the XmlWriter disposes, it will 
+          // automatically end any elements of any depth
+        }
+      } // also closes the underlying stream
+
+      // output all the contents of the compressed file 
+      WriteLine("{0} contains {1:N0} bytes.",
+        filePath, new FileInfo(filePath).Length);
+
+      WriteLine($"The compressed contents:");
+      WriteLine(File.ReadAllText(filePath));
+
+      // read a compressed file
+      WriteLine("Reading the compressed XML file:");
+      file = File.Open(filePath, FileMode.Open);
+
+      Stream decompressor;
+
+      if (useBrotli)
+      {
+        decompressor = new BrotliStream(file, CompressionMode.Decompress);
+      }
+      else
+      {
+        decompressor = new GZipStream(file, CompressionMode.Decompress);
+      }
+
+      using (decompressor)
+      {
+        using (XmlReader reader = XmlReader.Create(decompressor))
+        {
+          while (reader.Read()) // read the next XML node
+          {
+            // check if we are on an element node named callsign 
+            if ((reader.NodeType == XmlNodeType.Element)
+              && (reader.Name == "callsign"))
+            {
+              reader.Read(); // move to the text inside element 
+              WriteLine($"{reader.Value}"); // read its value
+            }
+          }
+        }
+      }
+    }
+    }
+}
