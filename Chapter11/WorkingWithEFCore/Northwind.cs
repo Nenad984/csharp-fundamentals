d30b230894310cf9dea523c26d4db2e3024ebf7e
Nenad984 <risticnenad.vr@gmail.com>
2021-04-12 12:55:59 +0000

    Working with Entity Framework

---
+++ Chapter11/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WorkingWithEFCore
+{
+    public class Northwind : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string path = System.IO.Path.Combine(
+                System.Environment.CurrentDirectory, "Northwind.db");
+
+            optionsBuilder.UseSqlite($"Filename={path}");     
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .Property(category => category.CategoryName)
+                .IsRequired() // NOT NULL
+                .HasMaxLength(15);
+        }
+    }
+}
