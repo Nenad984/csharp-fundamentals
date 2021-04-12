7e779942e7ef017daea5d362804e9922600c74c9
Nenad984 <risticnenad.vr@gmail.com>
2021-04-12 07:50:29 +0000

    Working with protecting data, salts, hashes

---
+++ Chapter10/CryptographyLib/Protector.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Xml.Linq;
+
+using static System.Console;
+
+namespace CryptographyLib
+{
+    public static class Protector
+    {
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+
+        private static readonly int iterations = 2000;
+
+        private static Dictionary<string, User> Users =
+            new Dictionary<string, User>();
+
+        public static string Encrypt(string plainText, string password)
+        {
+            byte[] encryptedBytes;
+
+            byte[] plainBytes = Encoding.Unicode
+            .GetBytes(plainText);
+
+            var aes = Aes.Create();
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+
+            aes.Key = pbkdf2.GetBytes(32);
+
+            aes.IV = pbkdf2.GetBytes(16);
+
+            using(var ms = new MemoryStream())
+            {
+                using(var cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(plainBytes, 0, plainBytes.Length);
+                }
+                encryptedBytes = ms.ToArray();
+               
+            }
+             return Convert.ToBase64String(encryptedBytes);
+        }
+
+        public static string Decrypt(string criptoText, string password)
+        {
+            byte[] plainBytes;
+
+            byte[] cryptoBytes = Convert
+                .FromBase64String(criptoText);
+
+            var aes = Aes.Create();
+
+            var pbkdf2 = new Rfc2898DeriveBytes(
+                password, salt, iterations);  
+
+            aes.Key = pbkdf2.GetBytes(32);
+
+            aes.IV = pbkdf2.GetBytes(16);
+
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(
+                    ms, aes.CreateDecryptor(),
+                    CryptoStreamMode.Write))
+                {
+                   cs.Write(cryptoBytes, 0, cryptoBytes.Length); 
+                }
+                plainBytes = ms.ToArray();
+            }
+                return Encoding.Unicode.GetString(plainBytes);
+        }
+
+        public static User Register(string username, string password)
+        {
+            // generate a random salt
+            var rng = RandomNumberGenerator.Create();
+
+            var saltBytes = new byte[16];
+
+            rng.GetBytes(saltBytes);
+            
+            var saltText = Convert.ToBase64String(saltBytes);
+
+            var saltedhashedPassword = SaltAndHashPassword(password, saltText);
+
+            var user = new User
+            {
+            Name = username, Salt = saltText,
+            SaltedHashedPassword = saltedhashedPassword
+            };
+
+            Users.Add(user.Name, user);
+
+            return user;
+        }
+
+        public static bool CheckPassword(string username, string password)
+        {
+            if (!Users.ContainsKey(username))
+            {
+                 return false;
+            }
+
+            var user = Users[username];
+
+            var saltedhashedPassword = SaltAndHashPassword(
+                password, user.Salt);
+
+            return (saltedhashedPassword == user.SaltedHashedPassword);     
+        }
+
+        private static string SaltAndHashPassword(string password, string salt)
+        {
+            var sha = SHA256.Create();
+
+            var saltedPassword = password + salt;
+
+            return Convert.ToBase64String(
+                sha.ComputeHash(Encoding.Unicode.GetBytes(
+                saltedPassword)));
+        }
+    }
+}
---
+++ Chapter10/EncryptionApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CryptographyLib;
+
+using static System.Console;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a message that you want to encrypt: ");
+
+            string message = ReadLine();
+
+            Write("Enter a password: ");
+
+            string password = ReadLine();
+
+            string cryptoText = Protector.Encrypt(message, password);
+
+            WriteLine($"Encrypted text: {cryptoText}");
+
+            Write("Enter the password: ");
+
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password);
+                WriteLine($"Decrypted text: {clearText}");
+            }        
+            catch (Exception ex)
+            {
+                WriteLine("Non-cryptographic exception: {0}, {1}",
+                arg0: ex.GetType().Name,
+                arg1: ex.Message);
+            }
+        }
+    }
+}
