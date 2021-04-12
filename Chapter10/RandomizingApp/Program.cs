54c0d659270c1da91ee297f545bbc1ba2d6fc20d
Nenad984 <risticnenad.vr@gmail.com>
2021-04-12 09:59:42 +0000

    Generating random numbers for cryptography

--- Chapter10/RandomizingApp/Program.cs
+++ Chapter10/RandomizingApp/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using CryptographyLib;
 
+using static System.Console;
+
 namespace RandomizingApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Write("How big do you want the key (in bytes): ");
+           string size = ReadLine();
+
+           byte[] key = Protector.GetRandomKeyOrIV(int.Parse(size));
+
+           WriteLine($"Key as byte array:");
+
+           for (int b = 0; b < key.Length; b++)
+           {
+               Write($"{key[b]:x2} ");
+               if (((b + 1) % 16) == 0) WriteLine();
+           }
+           WriteLine();
         }
     }
 }
