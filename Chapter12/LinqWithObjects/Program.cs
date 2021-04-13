0f99586201071363742c7c7b5835f8e02115a3fc
Nenad984 <risticnenad.vr@gmail.com>
2021-04-13 20:35:41 +0200

    Working with LINQ

---
+++ Chapter12/LinqWithObjects/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq; // for use extension methods, e.g Where, Select
+
+
+using static System.Console;
+
+namespace LinqWithObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqWithArrayOfStrings();
+        }
+
+        static void LinqWithArrayOfStrings()
+        {
+            var names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
+
+            var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+
+            foreach (string item in query)
+            {
+                WriteLine(item);
+            }
+        }
+
+        static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
+        }
+    }
+}
