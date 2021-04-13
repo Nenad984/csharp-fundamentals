a4d03237f889cf70647bc653aed08550b4f1e16f
Nenad984 <risticnenad.vr@gmail.com>
2021-04-13 22:27:55 +0200

    Working with OfType

--- Chapter12/LinqWithObjects/Program.cs
+++ Chapter12/LinqWithObjects/Program.cs
@@ -10,14 +10,20 @@ namespace LinqWithObjects
     {
         static void Main(string[] args)
         {
-            LinqWithArrayOfStrings();
+            // LinqWithArrayOfStrings();
+            LinqWithArrayOfExceptions();
         }
 
         static void LinqWithArrayOfStrings()
         {
             var names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
 
-            var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+            // var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+            // var query = names.Where(NameLongerThanFour);
+
+            var query = names.Where(name => name.Length > 4)
+                        .OrderBy(name => name.Length)
+                        .ThenBy(name => name);
 
             foreach (string item in query)
             {
@@ -28,6 +34,29 @@ namespace LinqWithObjects
         static bool NameLongerThanFour(string name)
         {
             return name.Length > 4;
+        }
+
+        static void LinqWithArrayOfExceptions()
+        {
+            var errors = new Exception[]
+            {
+                 new ArgumentException(),
+                 new SystemException(), 
+                 new IndexOutOfRangeException(), 
+                 new InvalidOperationException(),
+                 new NullReferenceException(), 
+                 new InvalidCastException(), 
+                 new OverflowException(), 
+                 new DivideByZeroException(),
+                 new ApplicationException()
+            };
+
+            var numberErrors = errors.OfType<ArithmeticException>();
+
+            foreach(var error in numberErrors)
+            {
+                WriteLine(error);
+            }
         }
     }
 }
