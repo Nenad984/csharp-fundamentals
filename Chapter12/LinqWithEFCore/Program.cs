fd9417cc1284c2213c799940aa1aa47c01e17ae6
Nenad984 <risticnenad.vr@gmail.com>
2021-04-14 21:36:28 +0200

    Working with grouping and joining data

--- Chapter12/LinqWithEFCore/Program.cs
+++ Chapter12/LinqWithEFCore/Program.cs
@@ -11,15 +11,23 @@ namespace LinqWithEFCore
     {
         static void Main(string[] args)
         {
-            FilterAndSort();
+            // FilterAndSort();
+           // JoinCategoriesAndProducts();
+           GroupJoinCategoriesAndProducts();
         }
 
         static void FilterAndSort()
         {
             using (var db = new Northwind())
             {
-                var query = db.Products.Where(product => product.UnitPrice < 10M)
-                            .OrderByDescending(product => product.UnitPrice);
+                var query = db.Products.Where(product => product.UnitPrice < 10)
+                           // .OrderByDescending(product => product.UnitPrice);
+                           .Select(product => new
+                           {
+                               product.ProductID,
+                               product.ProductName,
+                               product.UnitPrice
+                           });
 
                 WriteLine("Products that cost less than $10:");
 
@@ -27,8 +35,56 @@ namespace LinqWithEFCore
                 {
                     WriteLine("{0}: {1} costs {2:$#,##0.00}",
                     item.ProductID, item.ProductName, item.UnitPrice);
-                }  
-                WriteLine();          
+                }
+                WriteLine();
+            }
+        }
+
+        static void JoinCategoriesAndProducts()
+        {
+            using (var db = new Northwind())
+            {
+                var queryJoin = db.Categories.Join(
+                    inner: db.Products,
+                    outerKeySelector: category => category.CategoryID,
+                    innerKeySelector: product => product.CategoryID,
+                    resultSelector: (c, p) =>
+                    new { c.CategoryName, p.ProductName, p.ProductID }
+                ).OrderBy(cp => cp.ProductID);
+
+                foreach (var item in queryJoin)
+                {
+                    WriteLine("{0}: {1} is in {2}.", arg0: item.ProductID,
+                        arg1: item.ProductName,
+                        arg2: item.CategoryName);
+                }
+            }
+        }
+
+        static void GroupJoinCategoriesAndProducts()
+        {
+            using (var db = new Northwind())
+            {
+                var queryGroup = db.Categories.AsEnumerable().GroupJoin(
+                    inner: db.Products,
+                    outerKeySelector: category => category.CategoryID,
+                    innerKeySelector: product => product.CategoryID,
+                    resultSelector: (c, matchingProducts) => new
+                    {
+                        c.CategoryName,
+                        Products = matchingProducts.OrderBy(p => p.ProductName)
+                    }
+                );
+
+                foreach (var item in queryGroup)
+                {
+                    WriteLine("{0} has {1} products.", arg0: item.CategoryName, arg1: item.Products.Count());
+
+                    foreach (var product in item.Products)
+                    {
+                        WriteLine($" {product.ProductName}");
+                    }
+                }
             }
         }
     }
