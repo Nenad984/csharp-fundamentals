3917b2fcb656b4590705060942803b49d8c45da8
Nenad984 <risticnenad.vr@gmail.com>
2021-04-15 18:20:55 +0200

    Working with LINQ to XML

--- Chapter12/LinqWithEFCore/Program.cs
+++ Chapter12/LinqWithEFCore/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MyLibrary.Shared;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Xml.Linq;
 
 using static System.Console;
 
@@ -12,10 +13,12 @@ namespace LinqWithEFCore
         static void Main(string[] args)
         {
             //FilterAndSort();
-           // JoinCategoriesAndProducts();
-          // GroupJoinCategoriesAndProducts();
-          //AggregateProducts();
-          CustomExtensionMethods();
+            // JoinCategoriesAndProducts();
+            // GroupJoinCategoriesAndProducts();
+            //AggregateProducts();
+            // CustomExtensionMethods();
+            // OutputProductsAsXml();
+            ProcessSettings();
         }
 
         static void FilterAndSort()
@@ -94,7 +97,7 @@ namespace LinqWithEFCore
 
         static void AggregateProducts()
         {
-            using(var db = new Northwind())
+            using (var db = new Northwind())
             {
                 WriteLine("{0,-25} {1,10}",
                     arg0: "Product count:",
@@ -102,48 +105,83 @@ namespace LinqWithEFCore
 
                 WriteLine("{0,-25} {1,10:$#,##0.00}",
                     arg0: "Highest product price:",
-                    arg1: db.Products.Max(p => p.UnitPrice));    
+                    arg1: db.Products.Max(p => p.UnitPrice));
 
-                WriteLine("{0,-25} {1,10:N0}",arg0: "Sum of units in stock:",
-                    arg1: db.Products.Sum(p => p.UnitsInStock));  
+                WriteLine("{0,-25} {1,10:N0}", arg0: "Sum of units in stock:",
+                    arg1: db.Products.Sum(p => p.UnitsInStock));
 
                 WriteLine("{0,-25} {1,10:N0}",
                     arg0: "Sum of units on order:",
-                    arg1: db.Products.Sum(p => p.UnitsOnOrder));   
+                    arg1: db.Products.Sum(p => p.UnitsOnOrder));
 
                 WriteLine("{0,-25} {1,10:$#,##0.00}",
                     arg0: "Average unit price:",
-                    arg1: db.Products.Average(p => p.UnitPrice));  
+                    arg1: db.Products.Average(p => p.UnitPrice));
 
                 WriteLine("{0,-25} {1,10:$#,##0.00}",
                     arg0: "Value of units in stock:",
                     arg1: db.Products.AsEnumerable()
-                    .Sum(p => p.UnitPrice * p.UnitsInStock));         
+                    .Sum(p => p.UnitPrice * p.UnitsInStock));
             }
         }
 
         static void CustomExtensionMethods()
         {
-            using(var db = new Northwind())
+            using (var db = new Northwind())
             {
                 WriteLine("Mean units in stock: {0:N0}",
                     db.Products.Average(p => p.UnitsInStock));
 
                 WriteLine("Mean unit price: {0:$#,##0.00}",
-                    db.Products.Average(p => p.UnitPrice));   
+                    db.Products.Average(p => p.UnitPrice));
 
                 WriteLine("Median units in stock: {0:N0}",
-                    db.Products.Median(p => p.UnitsInStock)); 
+                    db.Products.Median(p => p.UnitsInStock));
 
                 WriteLine("Median unit price: {0:$#,##0.00}",
-                    db.Products.Median(p => p.UnitPrice)); 
+                    db.Products.Median(p => p.UnitPrice));
 
                 WriteLine("Mode units in stock: {0:N0}",
                     db.Products.Mode(p => p.UnitsInStock));
 
                 WriteLine("Mode unit price: {0:$#,##0.00}",
-                    db.Products.Mode(p => p.UnitPrice));           
+                    db.Products.Mode(p => p.UnitPrice));
             }
+        }
+
+        static void OutputProductsAsXml()
+        {
+            using (var db = new Northwind())
+            {
+                var productsForXml = db.Products.ToArray();
+                
+                var xml = new XElement("products", from p in productsForXml select 
+                        new XElement("product", 
+                            new XAttribute("id", p.ProductID), 
+                            new XAttribute("price", p.UnitPrice), 
+                            new XElement("name", p.ProductName)));
+                    
+                WriteLine(xml.ToString());
+            }
+        }
+
+        static void ProcessSettings()
+        {
+            XDocument doc = XDocument.Load("settings.xml");
+
+            var appSettings = doc.Descendants("appSettings")
+                .Descendants("add")
+                .Select(node => new
+                {
+                    Key = node.Attribute("key").Value,
+                    Value = node.Attribute("value").Value  
+                }).ToArray();
+
+            foreach(var item in appSettings)
+            {
+                WriteLine($"{item.Key}: {item.Value}");
+            }
+
         }
     }
 }
