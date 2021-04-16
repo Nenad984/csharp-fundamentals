b1dc40ccdc7b78cd5dfb0feead726d44cfb8e0ee
Nenad984 <risticnenad.vr@gmail.com>
2021-04-16 12:45:47 +0000

    Continuing with another task

--- Chapter13/WorkingWithTasks/Program.cs
+++ Chapter13/WorkingWithTasks/Program.cs
@@ -20,18 +20,33 @@ namespace WorkingWithTasks
             // MethodB();
 
             // MethodC();
+
+            WriteLine("Passing the result of one task as an input into another.");
+
+            var taskCallWebServiceAndThenStoredProcedure =
+                Task.Factory.StartNew(CallWebService)
+                .ContinueWith(previousTask =>
+                CallStoredProcedure(previousTask.Result));
+
+            WriteLine($"Result: {taskCallWebServiceAndThenStoredProcedure.Result}");
+
+
             WriteLine("Running methods asynchronously on multiple threads.");
 
-            Task taskA = new Task(MethodA);
+            // Task taskA = new Task(MethodA);
 
-            taskA.Start();
+           // taskA.Start();
 
-            Task taskB = Task.Factory.StartNew(MethodB);
+           // Task taskB = Task.Factory.StartNew(MethodB);
 
-            Task taskC = Task.Run(new Action(MethodC));
+            //Task taskC = Task.Run(new Action(MethodC)); -->
 
 
             WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+
+           // Task[] tasks = {taskA, taskB, taskC};
+
+           // Task.WaitAll(tasks);
         }
 
         static void MethodA()
@@ -59,6 +74,28 @@ namespace WorkingWithTasks
             Thread.Sleep(1000);
 
             WriteLine("Finished Method C.");
+        }
+
+        static decimal CallWebService()
+        {
+            WriteLine("Starting call to web service...");
+
+            Thread.Sleep((new Random()).Next(2000, 4000));
+
+            WriteLine("Finished call to web service.");
+
+            return 89.99M;
+        }
+
+        static string CallStoredProcedure(decimal amount)
+        {
+            WriteLine("Starting call to stored procedure...");
+
+            Thread.Sleep((new Random()).Next(2000, 4000));Thread.Sleep((new Random()).Next(2000, 4000));
+
+            WriteLine("Finished call to stored procedure.");
+
+            return $"12 products cost more than {amount:C}.";
         }
     }
 }
