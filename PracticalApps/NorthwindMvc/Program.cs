027f1b31426998e3b0e477441b6c383c978c5961
Nenad984 <risticnenad.vr@gmail.com>
2021-04-19 22:41:03 +0200

    Setting urls, working with Model in Index.cshtml

--- PracticalApps/NorthwindMvc/Program.cs
+++ PracticalApps/NorthwindMvc/Program.cs
@@ -21,7 +21,7 @@ namespace NorthwindMvc
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5000/");
+                    webBuilder.UseUrls("http://0.0.0.0:5000/", "http://localhost:5000", "https://localhost:5001");
                 });
     }
 }
