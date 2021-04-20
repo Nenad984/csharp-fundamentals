177d97066f44019c842147e25fa77491ed5e0020
Nenad984 <risticnenad.vr@gmail.com>
2021-04-20 22:55:14 +0200

    Working with REST API

---
+++ PracticalApps/NorthwindService/Startup.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using MyLibrary.Shared;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using NorthwindService.Repositories;
+
+using static System.Console;
+
+namespace NorthwindService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string databasePath = Path.Combine("..", "Northwind.db");
+
+            services.AddDbContext<Northwind>(options =>
+            options.UseSqlite($"Data Source={databasePath}"));
+
+            services.AddControllers(options =>
+            {
+                WriteLine("Default output formatters:");
+
+                foreach (IOutputFormatter formatter in options.OutputFormatters)
+                {
+                    var mediaFormatter = formatter as OutputFormatter;
+
+                    if (mediaFormatter == null)
+                    {
+                        WriteLine($" {formatter.GetType().Name}");
+                    }
+                    else
+                    {
+                        WriteLine(" {0}, Media types: {1}", arg0: mediaFormatter.GetType().Name, arg1: string.Join(", ",
+                            mediaFormatter.SupportedMediaTypes));
+                    }
+                }
+            })
+            .AddXmlDataContractSerializerFormatters()
+            .AddXmlSerializerFormatters()
+            .SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Northwind Service API", Version = "v1" });
+            });
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind Service API Version 1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
